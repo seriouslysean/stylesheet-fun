a284353d63eb1d05d6a72fc8bc0f71226d77acda
Sean Kennedy <sean@codeserious.com>
2022-06-13 16:38:53 -0400

    Add support for multiple CSS files, resolves #2

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="theme">
+    <h1>Index</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ pages/theme/[name].vue
@@ -0,0 +1,37 @@
+<script setup>
+const route = useRoute();
+const theme = route.params?.name || 'default';
+useHead({
+  link: [
+    { rel: 'stylesheet', href: `/css/${theme}.css` },
+  ],
+});
+</script>
+
+<template>
+  <div class="theme">
+    <h1>Theme: {{ theme }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ThemePage",
+  async asyncData(res) {
+    console.log('!!!', res);
+    return {
+      theme: res.params?.name?.toLowerCase() || 'default',
+    };
+  },
+  head() {
+    return {
+      link: [
+        {
+          rel: "stylesheet",
+          href: `/css/${this.theme}.css`,
+        },
+      ],
+    };
+  },
+};
+</script>
