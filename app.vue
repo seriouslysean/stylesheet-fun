acada178215890c6beb6ae2a1ded04b5bfb0c465
Sean Kennedy <sean@codeserious.com>
2022-06-13 16:42:59 -0400

    Add support for multiple CSS files, resolves #2 (#13)
    * Add support for multiple CSS files, resolves #2
    
    * Removing index script block
    
    * Removing duplicate theme page script block

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
+<script setup>
+useHead({
+  title: 'Stylesheet Fun',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    {
+      name: 'description',
+      content: `Aren't stylesheets fun?`,
+    },
+  ],
+  link: [
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
+  ],
+});
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div id="app">
+    <NuxtPage />
   </div>
 </template>
