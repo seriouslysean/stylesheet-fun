0ad1c3177bdcec07140a4ca9ab832ae6cd5d8f77
Sean Kennedy <sean@codeserious.com>
2022-06-13 16:41:34 -0400

    Removing duplicate theme page script block

--- pages/theme/[name].vue
+++ pages/theme/[name].vue
@@ -13,25 +13,3 @@ useHead({
     <h1>Theme: {{ theme }}</h1>
   </div>
 </template>
-
-<script>
-export default {
-  name: "ThemePage",
-  async asyncData(res) {
-    console.log('!!!', res);
-    return {
-      theme: res.params?.name?.toLowerCase() || 'default',
-    };
-  },
-  head() {
-    return {
-      link: [
-        {
-          rel: "stylesheet",
-          href: `/css/${this.theme}.css`,
-        },
-      ],
-    };
-  },
-};
-</script>
