ae7c3344c0dde0f52da77ad9812d5948476bf122
seriouslysean <seriouslysean@users.noreply.github.com>
2022-06-14 20:57:50 +0000

    deploy: a85afd596acbde55d4daf3a6a7a600321a429bc9

---
+++ _nuxt/index-348f6d2b.mjs
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c,a as o}from"./entry-f95422ee.mjs";const t={},n={class:"index"},_=o("h1",null,"Index",-1),a=[_];function r(d,i){return s(),c("div",n,a)}var f=e(t,[["render",r]]);export{f as default};
