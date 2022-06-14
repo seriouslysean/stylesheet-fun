c1b8bbdc7070002b4ff0cb01110b49cfdfd1c55b
Sean Kennedy <sean@codeserious.com>
2022-06-14 15:46:11 -0400

    ESlint adjustments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true,
-    },
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        parser: '@typescript-eslint/parser',
-    },
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:nuxt/recommended',
-        'plugin:vue/vue3-recommended',
-    ],
-    plugins: ['@typescript-eslint'],
-    rules: {
-        'comma-dangle': ['error', 'always-multiline'],
-        'semi-style': ['error', 'last'],
-        // allow use of script setup notation
-        'vue/script-setup-no-uses-vars': 'off',
-    },
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:nuxt/recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'indent': [2, 2],
+    'semi-style': ['error', 'last'],
+    // allow use of script setup notation
+    'vue/script-setup-no-uses-vars': 'off',
+  },
 }
