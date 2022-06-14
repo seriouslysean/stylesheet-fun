52c4026dfb764d14416191bbb4f61cb6bc761bc0
Sean Kennedy <sean@codeserious.com>
2022-06-14 15:45:56 -0400

    Dynamically generate route table based on css themes, resolves #14

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
+
+import { generateThemeRoutes } from './utils/router';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    typescript: {
-        shim: false,
+  typescript: {
+    shim: false,
+  },
+  hooks: {
+    'pages:extend' (routes) {
+      generateThemeRoutes(routes);
     },
+  },
 });
---
+++ utils/router.ts
@@ -0,0 +1,19 @@
+import { resolve } from 'path';
+
+export function generateThemeRoutes(routes) {
+  // TODO: make this dynamic based on files that exist in public/css
+  // or maybe read the eventual scss files from something like /styles/themes/{themeName}.scss
+  const themes = [
+    'default',
+    'blue',
+  ];
+  themes.forEach(theme => routes.push({
+    name: `theme-${theme}`,
+    path: `/theme/${theme}`,
+    file: resolve('./components/theme.vue'),
+    children: [],
+    meta: {
+      theme,
+    },
+  }));
+}
